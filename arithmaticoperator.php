0fab8b10d3f88e2bb28c30aa32679cdafbf0994c
KKPANDEY261 <karanpandey261@gmail.com>
2025-04-21 12:38:45 +0530

    next update

---
+++ arithmaticoperator.php
@@ -0,0 +1,42 @@
+<?php
+    <h3> Arithmetic Operator </h3>
+    <center>
+    // Addition
+    $a = 6;
+    $b = 9;
+    echo "Addition = " . $a + $b . "<br>";
+// Substraction
+    $a = 78;
+    $b = 67;
+    echo "Substraction = " . $a - $b . "<br>";
+    // Multipication
+    $a = 8;
+    $b = 9;
+    echo "Multiplication = " . $a * $b . "<br>";
+//Division
+    $a = 72;
+    $b = 9;
+    echo "Division = " . $a / $b . "<br>";
+    //Modulus
+    $a = 8;
+    $b = 9;
+    echo "Modulus = " . $a % $b . "<br>";
+
+    
+     //Exponentiation
+    $a = 8;
+    $b = 4;
+    echo "Exponentiation = " . $a ** $b . "<br>";
+//Increment
+    $a = 8;
+    echo "Increment = " . $a ++ . "<br>";  // post increment
+    echo "Increment = " . $a . "<br>";
+    echo "Increment = " .++ $a . "<br>"; // pri increment
+//Decrement
+    $b = 9;
+    echo "Decrement = " . $b-- . "<br>";
+    echo "Decrement = " . $b . "<br>";
+    echo "Decrement = " .-- $b . "<br>";
+    
+</center>
+?>
